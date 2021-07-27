07df818f3fdcaed669365c5e8f74d0c1c7378c47
hernancely <hernan.cely@hotmail.es>
2021-07-27 17:22:12 -0500

    se arreglo el texto en el readme

---
+++ src/main/java/com/producto/demoProductos/producto/service/ProductoService.java
@@ -0,0 +1,32 @@
+package com.producto.demoProductos.producto.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import com.producto.demoProductos.producto.model.Producto;
+import com.producto.demoProductos.producto..ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service
+
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    public Producto add(Producto producto){
+        return productoRepository.save(producto);
+    }
+
+    public List<Producto> getAll(){
+        return productoRepository.findAll();
+    }
+
+    public Producto update(int id, Producto producto){
+        return productoRepository.save(producto);
+    }
+
+    public void delete(int id){ productoRepository.deleteById(id);
+    }
+
+}
