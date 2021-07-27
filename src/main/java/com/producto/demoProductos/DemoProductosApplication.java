52c39adbc76a8c7c43c612be490eda15c99e274c
hernancely <hernan.cely@hotmail.es>
2021-07-27 16:27:41 -0500

    se crea el proyecto base

---
+++ src/main/java/com/producto/demoProductos/DemoProductosApplication.java
@@ -0,0 +1,13 @@
+package com.producto.demoProductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/producto/demoProductos/DemoProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.producto.demoProductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
